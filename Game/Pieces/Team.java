e3ed9a9720537bcb8b5ad6745596f512a6cfa25a
LiterallyAFlower <jonasparson03@gmail.com>
2025-01-08 17:13:46 -0500

    team

--- Game/Pieces/Team.java
+++ Game/Pieces/Team.java
@@ -1,5 +1,22 @@
 package Chess.Game.Pieces;
 
-public enum Team {
-
+public enum Team{
+    WHITE{
+        @Override
+        public String toString() {
+            return "White";
+        }
+    },
+    BLACK {
+        @Override
+        public String toString() {
+            return "Black";
+        }
+    },
+    NOCOLOR {
+        @Override
+        public String toString() {
+            return "No Color";
+        }
+    };
 }
