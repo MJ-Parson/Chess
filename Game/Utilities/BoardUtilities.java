0388436ad8dfd91a99ef99fb2199a25153ab6aed
LiterallyAFlower <jonasparson03@gmail.com>
2025-01-09 17:07:46 -0500

    .

--- Game/Utilities/BoardUtilities.java
+++ Game/Utilities/BoardUtilities.java
@@ -1,5 +1,13 @@
 package Chess.Game.Utilities;
 
+import Chess.Game.Pieces.Coordinate;
+
 public class BoardUtilities {
     
+    public static boolean isValidCoordinate(Coordinate coord) 
+    {
+        return(coord.getX() >= 0 && coord.getX() <= 7 && coord.getY() >= 0 && coord.getY() <= 7);
+    }
+
+ 
 }
