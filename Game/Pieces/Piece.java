e4a4ce430c0ad5ba66fd80a27fdb46216f8fbfb7
LiterallyAFlower <jonasparson03@gmail.com>
2025-01-09 13:30:29 -0500

    .

--- Game/Pieces/Piece.java
+++ Game/Pieces/Piece.java
@@ -1,8 +1,8 @@
 package Chess.Game.Pieces;
 
-import Chess.Game.Move;
 import Chess.Game.Gameboard.Board;
 import Chess.Game.Gameboard.Space;
+import Chess.Game.Move.Move;
 
 import java.util.List;
 
