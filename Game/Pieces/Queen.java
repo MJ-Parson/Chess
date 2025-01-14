76917942ae33dbba75fc722af21b1ac6f012ea15
LiterallyAFlower <jonasparson03@gmail.com>
2025-01-14 10:07:59 -0500

    .

---
+++ Game/Pieces/Queen.java
@@ -0,0 +1,47 @@
+package Chess.Game.Pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Chess.Game.Gameboard.Board;
+import Chess.Game.Gameboard.Space;
+import Chess.Game.Move.Move;
+import Chess.Game.Resources.PIECE_Configurations;
+import Chess.Game.Utilities.BoardUtilities;
+
+public class Queen extends Piece{
+    
+    public Queen(Team team) {
+        super(team, PieceTypes.QUEEN);
+    }
+
+    @Override
+    public List<Move> availableMoves(Board board, Coordinate currentCoord) {
+
+        List<Move> possibleMoves = new ArrayList<Move>();
+        Space currentSpace = board.getSpace(currentCoord);
+        Space endSpace;
+        Coordinate endCoordinate;
+
+        for(Coordinate coord : PIECE_Configurations.QUEEN_MOVES) {
+            endCoordinate = currentCoord;
+            
+            while(BoardUtilities.isValidCoordinate(endCoordinate.plus(coord))) {
+                endCoordinate = endCoordinate.plus(coord);
+                endSpace = board.getSpace(endCoordinate);
+                
+                if(!endSpace.hasPiece()) {
+                    possibleMoves.add(new Move(board, currentSpace, endSpace));
+                } else {
+                    if(endSpace.getPiece().getTeam() != this.getTeam()) {
+                        possibleMoves.add(new Move(board,currentSpace,endSpace));
+                        break;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return possibleMoves;
+    }
+}
--- Game/Utilities/MoveUtilities.java
+++ Game/Utilities/MoveUtilities.java
@@ -1,5 +1,41 @@
 package Chess.Game.Utilities;
 
+import Chess.Game.Gameboard.Board;
+import Chess.Game.Gameboard.Space;
+import Chess.Game.Move.Move;
+import Chess.Game.Pieces.PieceTypes;
+import Chess.Game.Pieces.Team;
+import Chess.Game.Resources.PIECE_Configurations;
+import Chess.Game.Pieces.Coordinate;
+
 public class MoveUtilities {
     
+    public static boolean isValidMove(Board board, Space endSpace) {
+        if(!board.hasChosenSpace()) {
+            return false;
+        } 
+        for(Move m : board.getChosenSpace().getPiece().availableMoves(board, board.getChosenSpace().getCoordinate())) {
+            
+            if(m.getEndSpace().getCoordinate().equals(endSpace.getCoordinate())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean controlCheckState(Board board, Team team){
+        
+        Space endSpace;
+        Coordinate currentCoord = board.getCoordTeamPiece(team, PieceTypes.KING);
+
+        //is there a knight as danger
+        for(Coordinate coord : PIECE_Configurations.KNIGHT_MOVES) {
+            if(!BoardUtilities.isValidCoordinate(currentCoord.plus(coord))) {
+                continue;
+            }
+            endSpace = board.getSpace(currentCoord.plus(coord));
+
+            
+        }
+    }
 }
